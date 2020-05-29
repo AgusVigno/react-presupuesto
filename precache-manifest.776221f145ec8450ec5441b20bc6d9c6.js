6a50bf18d54c807e0aad4d98e8a13b0ca263ffd8
Agustin Vignolo <agustinvignolo@MacBook-Air-de-Agustin.local>
2020-05-29 00:52:35 -0300

    Updates

---
+++ precache-manifest.776221f145ec8450ec5441b20bc6d9c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d979520ce7789b4968fac406880a3a0f",
+    "url": "/react-presupuesto/index.html"
+  },
+  {
+    "revision": "fb77531d304349f98d2c",
+    "url": "/react-presupuesto/static/css/main.480fbee7.chunk.css"
+  },
+  {
+    "revision": "ac0ebc034a2066fb15d5",
+    "url": "/react-presupuesto/static/js/2.cb48b291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-presupuesto/static/js/2.cb48b291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb77531d304349f98d2c",
+    "url": "/react-presupuesto/static/js/main.af7b6968.chunk.js"
+  },
+  {
+    "revision": "c3eeab273de471b0d530",
+    "url": "/react-presupuesto/static/js/runtime-main.e99d162d.js"
+  }
+]);
